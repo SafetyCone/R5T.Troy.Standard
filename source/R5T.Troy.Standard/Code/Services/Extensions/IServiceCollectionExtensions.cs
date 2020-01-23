e902825f3239d09ad320ddb10c3267734b8b6713
MinexAutomation <minex.automation@gmail.com>
2020-01-22 16:53:16 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Troy.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Troy.Default;
+
+
+namespace R5T.Troy.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTemporaryDirectoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<ITemporaryDirectoryNameConvention, TemporaryDirectoryNameConvention>();
+
+            return services;
+        }
+    }
+}
