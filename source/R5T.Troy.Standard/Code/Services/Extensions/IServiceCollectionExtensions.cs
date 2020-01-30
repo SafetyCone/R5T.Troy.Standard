85f5a999211e1a21059260b80f379d65a2335094
MinexAutomation <minex.automation@gmail.com>
2020-01-30 01:39:20 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Troy.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Troy.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
 using R5T.Troy.Default;
 
 
@@ -9,11 +10,23 @@ namespace R5T.Troy.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="ITemporaryDirectoryNameConvention"/> service.
+        /// </summary>
         public static IServiceCollection AddTemporaryDirectoryNameConvention(this IServiceCollection services)
         {
-            services.AddSingleton<ITemporaryDirectoryNameConvention, TemporaryDirectoryNameConvention>();
+            services.AddDefaultTemporaryDirectoryNameConvention();
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ITemporaryDirectoryNameConvention"/> service.
+        /// </summary>
+        public static ServiceAction<ITemporaryDirectoryNameConvention> AddTemporaryDirectoryNameConventionAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITemporaryDirectoryNameConvention>(() => services.AddTemporaryDirectoryNameConvention());
+            return serviceAction;
         }
     }
 }
